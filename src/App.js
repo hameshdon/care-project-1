65dd2e14df232a743f0f9ab6129f0765d869cee7
hamesh <hamesh30@gmail.com>
2022-09-19 12:12:44 +0530

    care_india_australia

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import CarePage from "./components/Pages/CarePage"
 import CarePageHome from './components/Pages/Page2/CarePageHome';
-import { BrowserRouter as Router, Routes, Route,useNavigate, useLocation  } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReadmoreOne from './components/Pages/Page2/ReadmorePages/ReadmoreOne';
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
     
             
   
-      <Route path='/carepagehome' element={<CarePageHome/>}/>
+      <Route path='/australia' element={<CarePageHome/>}/>
       <Route path='/readmoreone' element={<ReadmoreOne/>}/>
     </Routes>
     </Router>
